81ea19188292bebd477b781b1dbc14d331aedd58
Bouamar ELOUAHHABI <33551166+iambouamar@users.noreply.github.com>
2021-02-27 20:01:03 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import { Observable, of } from "rxjs";
+
+// Observable.create is deprecated,
+// use new Observable instead.
+// You can no longer .{operator},
+//  you need to .pipe({operator}) instead.
+
+let foo = of(1, 2, 3, 4, 5);
+
+function multiplyBy(number) {
+  const result = function (source) {
+    return new Observable((observer) => {
+      return source.subscribe({
+        next(x) {
+          observer.next(x * number);
+        },
+        error(err) {
+          observer.error(err);
+        },
+        complete() {
+          observer.complete();
+        }
+      });
+    });
+  };
+
+  return result;
+}
+
+let bar = foo.pipe(multiplyBy(100));
+
+bar.subscribe(
+  (x) => console.log("next " + x),
+  (err) => console.log("error " + err),
+  () => console.log("done")
+);
